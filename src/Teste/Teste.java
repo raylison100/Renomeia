eba8e7279ddd2cbf71b0085e61541fbf7c8df2b4
raylison.santos <raylison.santos@PCTA121544.tivit.corp>
2016-12-10 12:37:06 -0300

    2.3

---
+++ src/Teste/Teste.java
@@ -0,0 +1,22 @@
+package Teste;
+
+import java.io.File;
+
+public class Teste {
+	
+	public static void main(String[] args) throws Exception {
+		//   c:\fabio
+		String caminho = javax.swing.JOptionPane.showInputDialog("digite o diretorio a ser listado");
+		StringBuilder sb = new StringBuilder();
+		File raiz = new File(caminho);
+		for(File f: raiz.listFiles()) {
+			if(f.isFile()) {
+				System.out.println(f.getName());
+//				sb.append(f.getName());
+//				sb.append("\n");
+			}
+		}
+//		javax.swing.JOptionPane.showMessageDialog(null, sb.toString());
+	}
+
+}
